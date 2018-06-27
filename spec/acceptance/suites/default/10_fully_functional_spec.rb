f1b2d7a5b3cf8d0169cba7d08df262bd29609aa8
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-06-27 13:48:56 -0400

    (SIMP-4938) Update tests for new mate/dconf mods (#2)
    SIMP-4938 #comment x2go updated for new modules

--- spec/acceptance/suites/default/10_fully_functional_spec.rb
+++ spec/acceptance/suites/default/10_fully_functional_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper_acceptance'
 
-test_name 'x2go with MATE'
+test_name 'x2go with MATE and GNOME'
 
-describe 'x2go with MATE' do
-  let(:manifest) {
-    <<-EOS
-      class { 'x2go': server => true }
-      class { 'gnome': enable_mate => true }
-    EOS
-  }
-
+describe 'x2go with MATE and GNOME' do
   hosts.each do |host|
+    let(:manifest) {
+      _manifest = <<-EOS
+        include 'gnome'
+        class { 'x2go': server => true }
+      EOS
+
+      if host.host_hash[:roles].include?('mate_enabled')
+        _manifest << "\ninclude 'mate'"
+      end
+
+      _manifest
+    }
+
     context "on #{host}" do
       it 'should work with no errors' do
         apply_manifest_on(host, manifest, :catch_failures => true)
